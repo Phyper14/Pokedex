2719e3d98c25569018d34f0e7fa24b7d9bc4c3aa
Phyper14 <martins14.pablo@gmail.com>
2023-09-20 19:21:55 -0300

    feat: first load api

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const getPokemons = async () => {
+    let pokemonResponse = await (await fetch("https://pokeapi.co/api/v2/pokemon", { method: 'GET' })).json()
+    console.log(pokemonResponse)
+    let pokemons = pokemonResponse.results
+    let fixedPokemons = [];
+    // if (pokemons.length > 1) {
+    for (const key in pokemons) {
+        if (Object.hasOwnProperty.call(pokemons, key)) {
+            const element = pokemons[key];
+            let thisPokemonResponse = await (await fetch(element.url, { method: 'GET' })).json()
+            let thisPokemon = {}
+            thisPokemon.name = thisPokemonResponse.name
+            thisPokemon.order = thisPokemonResponse.order
+            thisPokemon.defaultSprite = thisPokemonResponse.sprites.front_default
+            fixedPokemons.push(thisPokemon)
+        }
+    }
+    return fixedPokemons
+}
+
+getPokemons().then((pokemons) => {
+    const main = document.getElementById("main")
+    console.log("main-> ", main)
+    console.log("POKEMONS -> ", pokemons)
+    pokemons.map((pokemon) => {
+        const pokemonImage = document.createElement("img")
+        pokemonImage.src = pokemon.defaultSprite
+        const pokemonName = document.createElement("h4")
+        pokemonName.innerHTML = `${pokemon.order} - ${pokemon.name}`
+        const thisElement = document.createElement("div")
+        thisElement.append(pokemonImage, pokemonName)
+        console.log("thisELEMENT -> ", thisElement)
+        main.append(thisElement)
+    })
+})
